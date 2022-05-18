e21abf51386a6665c5bead15034c3af9cb1c8b50
maui30 <103097103+maui30@users.noreply.github.com>
2022-05-18 16:29:31 +0800

    remove navbar scroll

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,3 @@
-// navbar
-
-const navbar = document.querySelector ('.navbar');
-
-window.addEventListener('scroll', () =>{
-    if(scrollY >= 180){
-        navbar.classList.add('bg');
-    }else{
-        navbar.classList.remove('bg');
-    }
-})
-
 //image collage
 
 const collageImages = [...document.querySelectorAll('.collage-img')]
