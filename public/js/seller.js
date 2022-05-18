f4d6da1427f6309e93f75766e3d6f5a73180f823
maui30 <103097103+maui30@users.noreply.github.com>
2022-05-18 21:30:19 +0800

    seller signup backend

---
+++ public/js/seller.js
@@ -0,0 +1,30 @@
+let loader=document.querySelector('.loader');
+let applyBtn=document.querySelector('.apply-btn');
+
+applyBtn.addEventListener('click',()=>{
+    let businessName=document.querySelector('#name').value;
+    let address=document.querySelector('#address').value;
+    let about=document.querySelector('#about').value;
+    let number=document.querySelector('#number').value;
+
+   if(!businessName.Length){
+        showFormError('Invalid Name');
+    }else if(!address.length ){
+        showFormError('Invalid address');
+    }else if(!about.Length){
+        showFormError('Invalid about');
+    }else if(number.length<10||!Number(number)){
+        showFormError('Invalid number');
+    }else{
+        // send data
+        loader.style.display='block';
+        sendData('/seller', {
+            name:businessName,
+            address:address,
+            about:about,
+            number:number,
+            email: JSON.parse(sessionStorage.user).email
+        })
+    }
+
+})
