0a7dad1e847768e6d5545735667e1c58b1117b10
maui30 <103097103+maui30@users.noreply.github.com>
2022-05-22 00:02:40 +0800

    add product interface

---
+++ public/js/add-product.js
@@ -0,0 +1,24 @@
+let user = JSON.parse(sessionStorage.user || null);
+
+window.onload = () => {
+    if(user == null){
+        location.replace('/login')
+    }
+}
+
+let editables = [...document.querySelectorAll('*[contenteditbale="true"]')];
+
+editables.map((element) => {
+    let placeholder = element.getAttribute('data-placeholder');
+    element.innerHTML = placeholder;
+    element.addEventListener('focus', () => {
+        if(element.innerHTML == placeholder){
+            element.innerHTML ='';
+        }
+    })
+    element.addEventListener('focusout',()=>{
+        if(!element.innerHTML.length){
+            element.innerHTML=placeholder;
+        }
+    })
+})
